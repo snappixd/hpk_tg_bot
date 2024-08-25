3d2a7a22bf7077777d4bd4b283b0720bab4ba6d4
snappixd <levinandrey33@gmail.com>
2024-08-25 16:46:18 +0300

    added scraper logic

--- main.go
+++ main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"hpk_tg_bot/scraper"
+	"log"
+)
 
+func main() {
+	log.Println("app start")
+
+	groupReps, err := scraper.ScrapGroupReps("КІ-232")
+	if err != nil {
+		log.Println(err.Error())
+	}
+
+	fmt.Printf("\t%s\n", groupReps.GroupName)
+	for _, rep := range groupReps.Replacements {
+		fmt.Printf("Пари: %s\nПредмет: %s\nВикладач: %s\nАудиторія: %s\n\n", rep.ParasNum, rep.Subject, rep.Teacher, rep.Classroom)
+	}
+
+	log.Println("app finish")
 }
--- scraper/scraper.go
+++ scraper/scraper.go
@@ -1,16 +1,54 @@
 package scraper
 
 import (
+	"errors"
 	"hpk_tg_bot/models"
 
 	"github.com/gocolly/colly"
 )
 
-func ScrapGroupReps(groupName string) models.GroupReplacement {
+func ScrapGroupReps(targerGroupName string) (models.GroupReplacement, error) {
 	c := colly.NewCollector()
+
+	var isTargerGroup bool
 
 	c.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36"
 
 	var groupRes models.GroupReplacement
 
+	c.OnHTML("tr:nth-child(n+3)", func(e *colly.HTMLElement) {
+		groupName := e.ChildText("td:nth-child(1)")
+
+		if groupName != "" {
+			isTargerGroup = groupName == targerGroupName
+			if isTargerGroup {
+				groupRes.GroupName = groupName
+			}
+		}
+
+		if isTargerGroup {
+			classroom := e.ChildText("td:nth-child(6)")
+			if classroom == "" {
+				classroom = "-"
+			}
+
+			replacement := models.Para{
+				ParasNum:  e.ChildText("td:nth-child(2)"),
+				Subject:   e.ChildText("td:nth-child(4)"),
+				Teacher:   e.ChildText("td:nth-child(5)"),
+				Classroom: classroom,
+			}
+
+			if replacement.ParasNum != "" || replacement.Subject != "" || replacement.Teacher != "" || replacement.Classroom != "" {
+				groupRes.Replacements = append(groupRes.Replacements, replacement)
+			}
+		}
+	})
+	c.Visit("https://hpk.edu.ua/replacements")
+
+	if len(groupRes.Replacements) == 0 {
+		return models.GroupReplacement{}, errors.New("заміни у вас відсутні")
+	}
+
+	return groupRes, nil
 }
